358075aca9945b5c7d74a47dc4b8519ee8c64d90
Muhammad Mudassir <116843475+Feynman-0@users.noreply.github.com>
2023-08-17 02:57:32 +0500

    Add files via upload

---
+++ day_14_of_code/script.js
@@ -0,0 +1,16 @@
+//1)Create a form element with 2 input fields for name and email. Add a submit button to it.
+/*<body>
+ <form>
+   <label for="name">Name:</label>
+   <input type="text" id="name" name="name" required>
+   
+   <label for="email">Email:</label>
+   <input type="text" id="email" email="email" required>
+   <button type="submit">Submit</button>
+ </form>*/
+//2)Write a CSS code to center align a text within a div tag.
+/*div {
+  text-align: center;
+}*/
+//3)Create an unordered list with 5 list items and add different colors to each item.
+
